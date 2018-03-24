dcaa65e3c552c36591579fbc128cbdf6aaa842ce
Elvin Shrestha <37188510+elwyncrestha@users.noreply.github.com>
2018-03-24 10:46:37 +0545

    Embedded Google calendar to Events

--- out/artifacts/InstituteManagementSystem_war_exploded/header.jsp
+++ out/artifacts/InstituteManagementSystem_war_exploded/header.jsp
@@ -21,7 +21,7 @@
                 </a>
             </li>
             <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Events">
-                <a class="nav-link" href="#">
+                <a class="nav-link" href="${pageContext.request.contextPath}/admin/events">
                     <i class="fa fa-fw fa-envelope-o"></i>
                     <span class="nav-link-text">Events</span>
                 </a>
---
+++ src/controller/backend/EventController.java
@@ -0,0 +1,21 @@
+package controller.backend;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/admin/events")
+public class EventController extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getRequestURI().equals(request.getContextPath()+"/admin/events")){
+            request.getRequestDispatcher("/events.jsp").forward(request,response);
+        }
+    }
+}
